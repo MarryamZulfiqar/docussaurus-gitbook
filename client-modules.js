6f853ad19cdcd3c385001034b3827858ed1d15ab
maryamzulfqar <maryam_xulfiqar@hotmail.com>
2022-03-11 15:51:14 +0500

    initial commit

---
+++ client-modules.js
@@ -0,0 +1,8 @@
+export default [
+  require("F:\\docusaurus - tutorial\\website\\node_modules\\remark-admonitions\\styles\\infima.css"),
+  require("F:\\docusaurus - tutorial\\website\\node_modules\\remark-admonitions\\styles\\infima.css"),
+  require("F:\\docusaurus - tutorial\\website\\node_modules\\remark-admonitions\\styles\\infima.css"),
+  require("F:\\docusaurus - tutorial\\website\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("F:\\docusaurus - tutorial\\website\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("F:\\docusaurus - tutorial\\website\\src\\css\\custom.css"),
+];
---
+++ routes.js
@@ -0,0 +1,146 @@
+
+import React from 'react';
+import ComponentCreator from '@docusaurus/ComponentCreator';
+export default [
+{
+  path: '/',
+  component: ComponentCreator('/','deb'),
+  exact: true,
+},
+{
+  path: '/__docusaurus/debug',
+  component: ComponentCreator('/__docusaurus/debug','3d6'),
+  exact: true,
+},
+{
+  path: '/__docusaurus/debug/config',
+  component: ComponentCreator('/__docusaurus/debug/config','914'),
+  exact: true,
+},
+{
+  path: '/__docusaurus/debug/content',
+  component: ComponentCreator('/__docusaurus/debug/content','c28'),
+  exact: true,
+},
+{
+  path: '/__docusaurus/debug/globalData',
+  component: ComponentCreator('/__docusaurus/debug/globalData','3cf'),
+  exact: true,
+},
+{
+  path: '/__docusaurus/debug/metadata',
+  component: ComponentCreator('/__docusaurus/debug/metadata','31b'),
+  exact: true,
+},
+{
+  path: '/__docusaurus/debug/registry',
+  component: ComponentCreator('/__docusaurus/debug/registry','0da'),
+  exact: true,
+},
+{
+  path: '/__docusaurus/debug/routes',
+  component: ComponentCreator('/__docusaurus/debug/routes','244'),
+  exact: true,
+},
+{
+  path: '/blog',
+  component: ComponentCreator('/blog','8eb'),
+  exact: true,
+},
+{
+  path: '/blog/first-blog-post',
+  component: ComponentCreator('/blog/first-blog-post','c34'),
+  exact: true,
+},
+{
+  path: '/blog/long-blog-post',
+  component: ComponentCreator('/blog/long-blog-post','25b'),
+  exact: true,
+},
+{
+  path: '/blog/mdx-blog-post',
+  component: ComponentCreator('/blog/mdx-blog-post','302'),
+  exact: true,
+},
+{
+  path: '/blog/tags',
+  component: ComponentCreator('/blog/tags','d04'),
+  exact: true,
+},
+{
+  path: '/blog/tags/docusaurus',
+  component: ComponentCreator('/blog/tags/docusaurus','a56'),
+  exact: true,
+},
+{
+  path: '/blog/tags/hello',
+  component: ComponentCreator('/blog/tags/hello','234'),
+  exact: true,
+},
+{
+  path: '/blog/tags/hola',
+  component: ComponentCreator('/blog/tags/hola','f65'),
+  exact: true,
+},
+{
+  path: '/markdown-page',
+  component: ComponentCreator('/markdown-page','be1'),
+  exact: true,
+},
+{
+  path: '/docs',
+  component: ComponentCreator('/docs','18a'),
+  
+  routes: [
+{
+  path: '/docs/intro',
+  component: ComponentCreator('/docs/intro','e84'),
+  exact: true,
+},
+{
+  path: '/docs/tutorial-basics/congratulations',
+  component: ComponentCreator('/docs/tutorial-basics/congratulations','7ef'),
+  exact: true,
+},
+{
+  path: '/docs/tutorial-basics/create-a-blog-post',
+  component: ComponentCreator('/docs/tutorial-basics/create-a-blog-post','2c8'),
+  exact: true,
+},
+{
+  path: '/docs/tutorial-basics/create-a-document',
+  component: ComponentCreator('/docs/tutorial-basics/create-a-document','f0d'),
+  exact: true,
+},
+{
+  path: '/docs/tutorial-basics/create-a-page',
+  component: ComponentCreator('/docs/tutorial-basics/create-a-page','ca5'),
+  exact: true,
+},
+{
+  path: '/docs/tutorial-basics/deploy-your-site',
+  component: ComponentCreator('/docs/tutorial-basics/deploy-your-site','508'),
+  exact: true,
+},
+{
+  path: '/docs/tutorial-basics/markdown-features',
+  component: ComponentCreator('/docs/tutorial-basics/markdown-features','f90'),
+  exact: true,
+},
+{
+  path: '/docs/tutorial-extras/manage-docs-versions',
+  component: ComponentCreator('/docs/tutorial-extras/manage-docs-versions','d64'),
+  exact: true,
+},
+{
+  path: '/docs/tutorial-extras/translate-your-site',
+  component: ComponentCreator('/docs/tutorial-extras/translate-your-site','16a'),
+  exact: true,
+},
+]
+},
+{
+  path: '*',
+  component: ComponentCreator('*')
+}
+];
